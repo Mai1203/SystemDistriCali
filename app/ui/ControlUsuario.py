d63fc18b71e81c6f36f179227a78e85309323589
Mai1203 <maicolcoral12@outlook.com>
2026-08-27 22:21:09 -0500

    feat: implement EgresoView UI components and add entry point for module testing

--- app/ui/ControlUsuario.py
+++ app/ui/ControlUsuario.py
@@ -318,7 +318,7 @@ class Ui_ControlUsuario(object):
 "    cursor: pointer; /* Cursor de mano al pasar sobre el botón */\n"
 "}")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(".\\../../assets/iconos/eliminar.png"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+        icon.addPixmap(QtGui.QPixmap("assets/iconos/eliminar.png"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
         self.BtnEliminar.setIcon(icon)
         self.BtnEliminar.setIconSize(QtCore.QSize(30, 30))
         self.BtnEliminar.setObjectName("BtnEliminar")
@@ -351,7 +351,7 @@ class Ui_ControlUsuario(object):
         self.horizontalLayout_3.addWidget(self.lineEdit)
         self.label_2 = QtWidgets.QLabel(parent=self.widget_2)
         self.label_2.setText("")
-        self.label_2.setPixmap(QtGui.QPixmap(".\\../../assets/iconos/buscar.png"))
+        self.label_2.setPixmap(QtGui.QPixmap("assets/iconos/buscar.png"))
         self.label_2.setObjectName("label_2")
         self.horizontalLayout_3.addWidget(self.label_2, 0, QtCore.Qt.AlignmentFlag.AlignHCenter)
         self.verticalLayout_2.addWidget(self.widget_2, 0, QtCore.Qt.AlignmentFlag.AlignHCenter)
